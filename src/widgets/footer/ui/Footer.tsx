e85a09d13b0bf9428980e08e0b67172c4d7331ff
evgen <agapoff.evgeny2014@ya.com>
2025-02-26 18:03:52 +0300

    updated Footer

--- src/widgets/footer/ui/Footer.tsx
+++ src/widgets/footer/ui/Footer.tsx
@@ -3,15 +3,17 @@ import './footer.scss';
 
 const currentYear = new Date().getFullYear();
 
-export function Footer(): React.ReactNode {
+export const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
         <div>{currentYear}</div>
-        <div className='footer-link'>
-          <Link target='_blank' to="https://github.com/evgagp">Github</Link>
+        <div className="footer-link">
+          <Link target="_blank" to="https://github.com/evgagp">
+            Github
+          </Link>
         </div>
       </div>
     </footer>
   );
-}
+};
